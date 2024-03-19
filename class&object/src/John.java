38ad2f4aacf7b8e802065900c25b7970b1fab091
xinyi2123 <162867143+xinyi2123@users.noreply.github.com>
2024-03-19 22:33:43 +0800

    Add files via upload

---
+++ class&object/src/John.java
@@ -0,0 +1,24 @@
+
+public class John {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 创建一个Person对象
+        person person1 = new person();
+        // 设置对象的属性
+        person1.name = "John";
+        person1.age = 30;
+        // 调用对象的方法
+        person1.displayInfo();
+
+        // 创建另一个Person对象
+        person person2 = new person();
+        // 设置对象的属性
+        person2.name = "Alice";
+        person2.age = 25;
+        // 调用对象的方法
+        person2.displayInfo();
+
+	}
+
+}
---
+++ class&object/src/person.java
@@ -0,0 +1,11 @@
+class person {
+    // 类的属性
+    String name;
+    int age;
+
+    // 类的方法
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
